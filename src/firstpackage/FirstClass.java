ffb9b83ddbb02cea603c09f12a7f2070ad261d12
Ekram <ekremkose2001@gmail.com>
2020-07-23 00:04:19 -0500

    confict olustu

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
 		System.out.println("Second commit from local repository");
-		System.out.println("Third commit from local repository");
+		System.out.println("3. commitimiz local repo'dannn");
 		System.out.println("Last commit from local repository");
 		System.out.println("First commit from remote repository");
 
