a85bd3080b373904e9995b35575fefb8b709a28b
ekremkose55 <67019372+ekremkose55@users.noreply.github.com>
2020-07-23 00:56:33 -0500

    Update FirstClass.java
    aaaa

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -9,22 +9,7 @@ public class FirstClass {
 		System.out.println("Last commit from local repository");
 		System.out.println("First commit from remote repository");
 		System.out.println("5. commitimiz remote repo'dannn");
-		System.out.println("6. commitimiz local repo'dannn");
-		System.out.println("7. commitimiz remote repo'dannn");
-		System.out.println("8. commitimiz local repo'dannn dan dandanakan");
-		System.out.println("8. hariciye kogusu hahahaha!");
-		System.out.println("9. hariciye kogusu olmasi gerekmiyor mu aptal!");
-		System.out.println("Haklisiniz bu bana hic yakismadi!");
-		System.out.println("Bu ne bencillik bana yakistirdiniz ama!");
-		System.out.println("Bunda da haklisiniz ozur dilerim hara ettim lutfen bagislayin!");
-		System.out.println("Ben kabalik yapmayacagim ozrunuz kabul oldu!");
-		System.out.println("Bu da bana ders olsun!");
-		System.out.println("Ne diyeyim dil belasi!");
-		System.out.println("Bu muhabbete son verelim mi uzadi sanki");
-		System.out.println("Neden olmasin ne demisler ya hayir konus ya da sus!");
-		System.out.println("Sustum!");
-		System.out.println("Aferin");
-
+		
 	}
 
 }
