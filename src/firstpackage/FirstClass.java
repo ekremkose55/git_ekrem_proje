9471e995e46387c4723b23403a6ac173ecd2551d
Ekram <ekremkose2001@gmail.com>
2020-07-23 23:36:36 -0500

    Son satirda degisti

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
 		System.out.println("Second commit from local repository");
-		System.out.println("4. commitimiz remote repo'dannn");
+		System.out.println("son degisim localden");
 		
 		
 		
