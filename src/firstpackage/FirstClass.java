bb200edf7cc3d0ade137afde9cbe598f912801c9
Ekram <ekremkose2001@gmail.com>
2020-07-23 23:34:16 -0500

    son satir silindi

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,7 +6,7 @@ public class FirstClass {
 		System.out.println("First commit from local repository");
 		System.out.println("Second commit from local repository");
 		System.out.println("4. commitimiz remote repo'dannn");
-		System.out.println("Last commit from local repository");
+		
 		
 		
 		
