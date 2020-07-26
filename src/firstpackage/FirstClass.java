2f68aad7c2e8db4728e3e9ecabe001bc9cdcbef9
Ekram <ekremkose2001@gmail.com>
2020-07-26 01:47:12 -0500

    son eklenenler

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -9,6 +9,12 @@ public class FirstClass {
 		System.out.println("son conflict remote den");
 		System.out.println("yeni bir commit daha remoteden");
 		System.out.println("son bir commit daha remoteden");
+		System.out.println("First commit from local repository");
+		System.out.println("Second commit from local repository");
+		System.out.println("son degisim localden");
+		System.out.println("son conflict remote den");
+		System.out.println("yeni bir commit daha remoteden");
+		System.out.println("son bir commit daha remoteden");
 
 		
 		
